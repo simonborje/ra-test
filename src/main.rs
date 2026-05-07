fa267b4b0f81f2c2584dc4f762ecc57fb97ade13
Simon Börjesson <simonborje@gmail.com>
2026-05-08 00:19:20 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#![no_std]
+#![no_main]
+
+
+use panic_halt as _;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) -> ! {
+    
+    loop {}
+}
+
+
+
+
+
+
+// // Array size HAS to be defined by const or panic will not happen
+// pub const MY_ARRAY_SIZE: usize = 5;
+
+// struct TestStuct {
+//     my_array: [u32; MY_ARRAY_SIZE],
+// }
+
+// impl TestStuct {
+//     // function has to be async or panic will not exist
+//     pub async fn error(&mut self) {
+//         let _ = self.my_array[0];
+//     }
+// }
