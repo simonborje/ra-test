58e56649daa8262ae1159cb6a43ad4331f44869b
Simon Börjesson <simonborje@gmail.com>
2026-05-08 00:28:00 +0200

    Add test code for second panic

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use panic_halt as _;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
-    
     loop {}
 }
 
@@ -16,15 +15,15 @@ async fn main(_spawner: Spawner) -> ! {
 
 
 // // Array size HAS to be defined by const or panic will not happen
-// pub const MY_ARRAY_SIZE: usize = 5;
+pub const MY_ARRAY_SIZE: usize = 5;
 
-// struct TestStuct {
-//     my_array: [u32; MY_ARRAY_SIZE],
-// }
+struct TestStuct {
+    my_array: [u32; MY_ARRAY_SIZE],
+}
 
-// impl TestStuct {
-//     // function has to be async or panic will not exist
-//     pub async fn error(&mut self) {
-//         let _ = self.my_array[0];
-//     }
-// }
+impl TestStuct {
+    // function has to be async or panic will not exist
+    pub async fn error(&mut self) {
+        let _ = self.my_array[0];
+    }
+}
